9004db823564f0b34a8bf22c916de03adb83b6b1
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2023-11-03 12:22:56 +0100

    installation

--- app.vue
+++ app.vue
@@ -1,21 +1,11 @@
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1> Where the real legends begin</h1>
   </div>
+
 </template>
 
-const runtimeConfig = useRuntimeConfig();
-const { data, error } = await useFetch('https://graphql.datocms.com', {
-    method: 'POST',
-    headers: {
-        Authorization: `Bearer ${runtimeConfig.public.datoCmsToken}`,
-    },
-    body: {
-        query: QUERY,
-    },
-    transform: ({ data, errors }) => {
-        if (errors) throw new errors;
 
-        return data;
-    },
-});
+
+
+
